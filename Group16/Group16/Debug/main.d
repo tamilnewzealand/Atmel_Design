60d2901827e01cf744571bc26e83c07b799e7e00
Sylvain Bechet <bechet.sylvain@gmail.com>
2016-08-08 01:06:56 +1200

    Debugging
    Part 2.

--- Group16/Group16/Debug/main.d
+++ Group16/Group16/Debug/main.d
@@ -10,6 +10,9 @@ main.d main.o: .././main.c .././prototypes16.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  .././funtions16.h
 
 .././prototypes16.h:
@@ -35,5 +38,11 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 .././funtions16.h:
--- Group16/Group16/prototypes16.h
+++ Group16/Group16/prototypes16.h
@@ -7,9 +7,9 @@
 
 #include <avr/io.h>
 #define F_CPU 16000000UL // Clock Speed
-//#define BAUD 9600
-//#include <util/delay.h>
-//#define MYUBRR ((F_CPU /(16UL*BAUD))-1)
+#define BAUD 9600
+#include <util/delay.h>
+#define MYUBRR ((F_CPU /(16UL*BAUD))-1)
 
 void uart_init ();
 void uart_transmit (uint8_t data );
