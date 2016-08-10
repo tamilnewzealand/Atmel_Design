e873805bbd01353a7a0a2d7f8ea8847486dcb5e4
Sylvain Bechet <bechet.sylvain@gmail.com>
2016-08-11 10:23:43 +1200

    Hex Transmission
    Array values transmitted in hex and are read in backwards binary.

--- Group16/Group16/main.c
+++ Group16/Group16/main.c
@@ -7,15 +7,23 @@
 
 #include "prototypes16.h"
 #include "funtions16.h"
+#include <util/delay.h>
 
 
 /* declare global variables here*/
 
 int main( void )
 {
+	uint8_t array[10] = {12,15,67,89,72,34,49,59,61,52};
+	
 	uart_init(MYUBRR);
 	while (1){
 		//read array
-		uart_transmit(0b00100111);
+		for(uint8_t i=0;i<10;i++) {
+			uint8_t variable = array[i];
+			uart_transmit(variable);
+			_delay_ms(500);
+		}
+		
 	}
 }
