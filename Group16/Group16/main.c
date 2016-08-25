1d077e032bc460c8ea96d40dcfba9384c199f8d6
Sylvain Bechet <bechet.sylvain@gmail.com>
2016-08-25 14:36:36 +1200

    Commenting
    Added comments on the next steps which will be covered by Nadeem. These
    include steps for the ADC and for the timer.

--- Group16/Group16/main.c
+++ Group16/Group16/main.c
@@ -14,7 +14,8 @@
 
 int main(void)
 {
-	//convert 10 bit input to decimal
+	/*
+	convert 10 bit input to decimal
 	
 	//compute power
 	
@@ -27,10 +28,62 @@ int main(void)
 		
 	//convert the created number to decimal
 	
-	//correctly input it in array (watch out for position)
+	//correctly input it in array (watch out for position) 
+	*/
 	
+	/*ADC POLLING STEPS
+	1. Make the pin for selected ADC channel an input pin
 	
+	2. Turn on ADC module of AVR because it is disabled upon power on reset to save power
 	
+	3. Select conversion speed using ADPS2 : 0 
+	
+	4. Select Voltage reference (use REFS0 and REFS1) and input channel (use Mux3 : 0 in ADMUX)
+	
+	5. Activate the start conversion bit by writing a one to ADSC bit of ADCSRA
+	
+	6. Wait for conversion to be completed by polling the ADIF bit ADCSRA register
+	
+	7. After the ADIF bit has gone high, read the ADCL and ADCH. Read ADCL before ADCH.
+	
+	8. If you want to read selected channel again, go back to step 5. 
+	
+	9. If you want to select another Vref source or input channel, go back to step 4
+	*/
+	
+	/*ADC INTERRUPTS STEPS
+	1. Make the pin for selected ADC channel an input pin 
+	
+	2. Turn on ADC module of AVR because it is disabled upon power on reset to save power
+	
+	3. Select conversion speed using ADPS2 : 0 
+	
+	4. Select Voltage reference (use REFS0 and REFS1) and input channel (use Mux4 : 0 in ADMUX)
+	
+	5. Set ADIE bit high 
+	
+	6. Activate the start conversion bit by writing a one to ADSC bit of ADCSRA
+	
+	7. Upon completion ADIF bit has gone high, (you can read the ADCL and ADCH in ISR)
+	
+	8. After the ADIF bit has gone high, it will force the CPU to jump to the ADC interrupt handler
+	*/
+	
+	/*TIMER POLLING STEPS
+	1. Load the TCNT0 with the initial value. 
+	
+	2. Load the value into TCCR0 register, indicating which mode is to be used and the prescaler option.
+	
+	3. Keep monitoring the timer overflow flag (TOV0) to see if it is raised. 
+	
+	4. Clear TOV0 flag for the next round. 
+	
+	5. Go back to step 1 to load TCNT0 again.
+	*/
+	
+	/*
+	Note code is available for polling and interrupts on Nadeem's sheet (number 5).
+	*/
 	
 	uint8_t array[4] = {102,71,40,9};
 	//prints out 1,2,3,4 in sequence from left to right on SSD in order
